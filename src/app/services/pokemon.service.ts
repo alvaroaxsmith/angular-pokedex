8bbdf4ad5e786ac487473e0a8fab7ec84ec510ea
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2022-11-14 23:32:25 -0300

    refactor: remove unused array

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PokemonService {
-  pokemons = [];
+
   constructor(private httpClient: HttpClient) {}
 
   getPokemons(): any {
