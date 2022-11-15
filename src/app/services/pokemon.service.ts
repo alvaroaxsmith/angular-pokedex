f182168f18abd732d5cbdb773488c337cfb5aac0
Álvaro Machado Ferreira <alvaromachadoferreira@hotmail.com>
2022-11-14 23:31:12 -0300

    refactor: remove comment

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,7 +8,6 @@ export class PokemonService {
   pokemons = [];
   constructor(private httpClient: HttpClient) {}
 
-  // create getfunction observable to get the pokemons
   getPokemons(): any {
     return this.httpClient.get('https://pokeapi.co/api/v2/pokemon?limit=151');
   }
